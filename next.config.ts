6e94fe7350bd8d33db00d34c71007bdefdac73cc
Davi <N4linkWiki>
2025-09-22 17:11:44 -0300

    feat: update for .env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/saas-barbearia",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         hostname: "utfs.io",
