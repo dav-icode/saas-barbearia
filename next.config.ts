06e2260a704e78ac7a911e98f5a090f113b9199d
Davi <N4linkWiki>
2025-09-23 15:39:31 -0300

    feat: resolution for problems

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
+// next.config.ts
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/saas-barbearia",
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         hostname: "utfs.io",
